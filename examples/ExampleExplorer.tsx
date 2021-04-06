6255cc2286bb48cc5a188b507d2cb00f83604e72
Tristan Dyer <atdyer@ncsu.edu>
2021-04-06 11:57:15 -0400

    Update examples to actually take advantage of sass

--- examples/ExampleExplorer.tsx
+++ examples/ExampleExplorer.tsx
@@ -1,10 +1,9 @@
 import { ComponentType, FC, useState } from 'react';
-import { Example0 } from '../Example0';
-import { Example1 } from '../Example1';
-import { Example2 } from '../Example2';
-import { Example3 } from '../Example3';
-import './example-explorer.scss';
-import { Example4 } from '../Example4';
+import { Example0 } from './Example0';
+import { Example1 } from './Example1';
+import { Example2 } from './Example2';
+import { Example3 } from './Example3';
+import { Example4 } from './Example4';
 
 const ExampleExplorer: FC = props => {
 
